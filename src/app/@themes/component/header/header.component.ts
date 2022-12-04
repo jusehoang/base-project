1c9890f723ca730d44fbea0a20cdfe9d01728aaf
jusehoang <jusehoang1999@gmail.com>
2022-12-04 23:04:18 +0700

    update header

--- src/app/@themes/component/header/header.component.ts
+++ src/app/@themes/component/header/header.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  isLogin = false;
+  totalPrice = 0;
+  totalItem = 0;
+  showLoading = true;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/@themes/component/theme.module.ts
+++ src/app/@themes/component/theme.module.ts
@@ -7,12 +7,15 @@ import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 const MODULES = [
   NzButtonModule,
   NzGridModule,
   NzMessageModule,
   NzModalModule,
+  NzIconModule,
+  NzInputModule
 ]
 
 @NgModule({
